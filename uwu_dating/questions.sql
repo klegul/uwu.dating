5d0cba72a0229a50e405f4ea06e3cbcc32510e27
klegul <gregor@klegul.dev>
2025-12-15 22:53:44 +0100

    39C3 style

--- uwu_dating/questions.sql
+++ uwu_dating/questions.sql
@@ -7,34 +7,8 @@ VALUES (1, 2, 'uwu');
 INSERT INTO answer_choice
 VALUES (1, 3, 'üwü');
 
+INSERT INTO question
+VALUES (2, 'how many hours should one engel?', 'number', 'lets troll');
 
 INSERT INTO question
-VALUES (2, 'what are the tickets called?', 'choice', 'Sure');
-INSERT INTO answer_choice
-VALUES (2, 1, 'Goulash Max Pro');
-INSERT INTO answer_choice
-VALUES (2, 2, 'Goulash Gourment Gift');
-INSERT INTO answer_choice
-VALUES (2, 3, 'Goulash Indulgence Package');
-INSERT INTO answer_choice
-VALUES (2, 4,
-        'I''d just like to interject for a moment. What you''re referring to as Goulash, is in fact, Entropia/Goulash, or as I''ve recently taken to calling it, Entropia plus Goulash');
-INSERT INTO answer_choice
-VALUES (2, 5, 'actionaly these are not tickets. don''t call it that');
-
-INSERT INTO question
-VALUES (3, 'how many hours should one troll?', 'number', 'lets engel');
-
-INSERT INTO question
-VALUES (4, 'what is the best workshop?', 'choice', 'send');
-INSERT INTO answer_choice
-VALUES (4, 1, 'The other kind of Rope Access for Nerds');
-INSERT into answer_choice
-VALUES (4, 2, 'Workshop for the cutest LED earrings ever');
-INSERT into answer_choice
-VALUES (4, 3, 'Spliceworkshop - Das Löten der Glasfaser');
-INSERT into answer_choice
-VALUES (4, 4, 'Platinendesign mit KiCad');
-
-INSERT INTO question
-VALUES (5, 'please describe everything you in believe in one sentence', 'text', 'send');
+VALUES (3, 'please describe everything you in believe in one sentence', 'text', 'send');
